5f98b53fc72be1f1216bd7b0f143cba13a401328
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-06-13 19:44:02 +0300

    comments end

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,16 +1,23 @@
 import express from "express";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { authenticateUser } from "../middlewares/middleware";
 import commentCtrl from "../controllers/commentController";
 
 const router = express.Router() as Router;
+//? Comments routes
+// Create a comment for a specific post
+router.post("/:postId", authenticateUser, commentCtrl.createComment);
+// This route is for deleting a specific comment from a post that I created. You can delete any comment in a specific post by using the postId and commentId.
+router.delete(
+  "/:postId/:commentId",
+  authenticateUser,
+  commentCtrl.deleteOneCommentFromMyPost
+);
+// Delete a specific comment by id (needs authentication for the user who created the comment)
+router.delete("/:commentId", authenticateUser, commentCtrl.deleteComment);
 
-router.get("/", commentCtrl.getAllComments); // Get all comments
+// Update a specific comment by its id
+router.patch("/:commentId", authenticateUser, commentCtrl.updateComment);
 
-router.post("/:postId", authenticateUser, commentCtrl.createComment); // Create a comment for a specific post
-
-router.patch("/:commentId", authenticateUser, commentCtrl.updateComment); // Update a specific comment by its id
-
-router.delete("/:commentId", authenticateUser, commentCtrl.deleteComment); // Delete a specific comment by its id
-
+// TODO:Get comments from a specific post
 export default router;
